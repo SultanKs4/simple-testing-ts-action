4aebf999ed828a31806e01ec1b1d7553d3ef5016
SultanKs4 <sultangendut@gmail.com>
2020-07-12 22:38:42 +0700

    add update testing

--- src/study.test.ts
+++ src/study.test.ts
@@ -4,4 +4,8 @@ describe("study testing", () => {
   it("Testing jest", () => {
     expect(2).equal(2);
   });
+
+  it("Can more than testing in 1 file", () => {
+    expect(6 + 4).equal(10);
+  });
 });
